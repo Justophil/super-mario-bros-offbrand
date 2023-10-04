5288fe4ce687b2a2b133d33bed863c6d17cd4e1c
phil xyz <89649491+Justophil@users.noreply.github.com>
2023-10-03 23:02:55 -0400

    small push

--- Super Mario Bros Off Brand/Assets/Scripts/CheckpointBehavior.cs
+++ Super Mario Bros Off Brand/Assets/Scripts/CheckpointBehavior.cs
@@ -7,7 +7,7 @@ public class CheckpointBehavior : MonoBehaviour
 {
     void OnTriggerEnter(Collider other) {
         if(other.tag == "Player") {
-            GameManager.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+            GameManager.LoadLevel(1);
         }    
     }
 }
